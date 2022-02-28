845e7c7ac0cd693cb21c06b028758a837ded5493
rafael <rafaelviniciuscardoso@gmail.com>
2022-02-28 13:45:26 -0300

    update name tag codedeploy

--- codedeploy_deployment_group/main.tf
+++ codedeploy_deployment_group/main.tf
@@ -1,6 +1,6 @@
 resource "aws_codedeploy_deployment_group" "codedeploy_group_stack" {
   app_name              = var.app_name
-  deployment_group_name = join("-", ["group", var.environment, var.name])
+  deployment_group_name = join("-", [var.deployment_group_name, var.environment, var.name])
   service_role_arn      = var.service_role_arn
 
   ec2_tag_set {
