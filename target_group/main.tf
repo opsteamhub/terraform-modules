c8325e51d39729467e6d2e22cde84533f7ab1081
rafael <rafaelviniciuscardoso@gmail.com>
2022-02-28 18:54:50 -0300

    update target group

--- target_group/main.tf
+++ target_group/main.tf
@@ -1,22 +1,15 @@
 resource "aws_lb_target_group" "tg" {
   name     = join("-", ["tg", var.environment, var.name])
-  name_prefix     = try(substr(var.name_prefix,0,6), null)
   port     = var.port
   protocol = var.protocol
   vpc_id   = var.vpc_id
 
-
-  dynamic "health_check" {
-    for_each = var.health_check != null ? [1] : []
-    content {
-      path                = try(var.health_check.path, null)
-      timeout             = var.health_check.timeout
-      matcher             = try(var.health_check.matcher, null)
-      interval            = var.health_check.interval
-      protocol            = try(var.health_check.protocol, null)
-      healthy_threshold   = try(var.health_check.healthy_threshold, null)
-      unhealthy_threshold = try(var.health_check.unhealthy_threshold, null)
-    }
+  health_check {
+    port     = var.port
+    protocol = var.health_check_protocol
+    path     = var.path
+    timeout  = 5
+    interval = 10
   }
 
   tags = {
--- target_group/variables.tf
+++ target_group/variables.tf
@@ -22,22 +22,3 @@ variable "health_check_protocol" {
   default = "HTTP"
 }
 
-variable "name_prefix" {
-  description = "(Optional, Forces new resource) Creates a unique name beginning with the specified prefix. Conflicts with name. Cannot be longer than 6 characters."
-  default     = null
-  type        = string
-}
-
-variable "health_check" {
-  description = "Health Check configuration block."
-  default     = null
-  type = object({
-    path                = string
-    timeout             = number
-    matcher             = optional(string)
-    interval            = number
-    protocol            = optional(string)
-    healthy_threshold   = optional(number)
-    unhealthy_threshold = optional(number)
-  })
-}  
