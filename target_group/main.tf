f1d72bd9654263365129e48c15861de2146a5919
rafael <rafaelviniciuscardoso@gmail.com>
2022-02-28 17:29:33 -0300

    update target group

--- target_group/main.tf
+++ target_group/main.tf
@@ -3,21 +3,14 @@ resource "aws_lb_target_group" "tg" {
   port     = var.port
   protocol = var.protocol
   vpc_id   = var.vpc_id
-  target_type = var.target_type
 
-  dynamic "health_check" {
-    for_each = var.health_check != null ? [1] : []
-    content {
-      path                = try(var.health_check.path, null)
-      timeout             = var.health_check.timeout
-      matcher             = try(var.health_check.matcher, null)
-      interval            = var.health_check.interval
-      protocol            = try(var.health_check.protocol, null)
-      healthy_threshold   = try(var.health_check.healthy_threshold, null)
-      unhealthy_threshold = try(var.health_check.unhealthy_threshold, null)
-    }
+  health_check {
+    port     = var.port
+    protocol = var.health_check_protocol
+    path     = var.path
+    timeout  = var.target_group_timeout
+    interval = var.target_group_interval
   }
-
 
   tags = {
     Name          = join("-", ["tg", var.environment, var.name])
