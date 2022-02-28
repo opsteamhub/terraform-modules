25f124e6d2b911df36217d0a4a4e8af3d88e325a
rafael <rafaelviniciuscardoso@gmail.com>
2022-02-28 17:25:15 -0300

    update target group

--- target_group/variables.tf
+++ target_group/variables.tf
@@ -8,24 +8,36 @@ variable "port" {}
 
 variable "protocol" {}
 
+variable "target_type" {
+  description = "Type of target that you must specify when registering targets with this target group. Valid values: ip|instance|lambda"
+  default     = "instance"
+  type        = string
+}
+
 variable "environment" {}
 
 variable "provisioned" {
   default = "Terraform"
 }
 
-variable "path" {
-  default = ""  
-}
-
-variable "health_check_protocol" {
-  default = "HTTP"
-}
-
-variable "target_group_timeout" {
-  default = ""
-}
-
-variable "target_group_interval" {
-  default = 10 
+variable "health_check" {
+  description = "Health Check configuration block."
+  default     = null
+  type = object({
+    path                = string
+    timeout             = number
+    matcher             = optional(string)
+    interval            = number
+    protocol            = optional(string)
+    healthy_threshold   = optional(number)
+    unhealthy_threshold = optional(number)
+  })
+  # Example:
+  # path                = "/"
+  # timeout             = 10
+  # matcher             = "200-399"
+  # interval            = 15
+  # protocol            = "HTTP"
+  # healthy_threshold   = 2
+  # unhealthy_threshold = 2
 }
