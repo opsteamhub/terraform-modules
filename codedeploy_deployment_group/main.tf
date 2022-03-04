40dfd0d79b1698e2e8f8e5ce0610c55bb9041fb6
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-04 16:37:16 -0300

    update name deployment group

--- codedeploy_deployment_group/main.tf
+++ codedeploy_deployment_group/main.tf
@@ -1,6 +1,7 @@
 resource "aws_codedeploy_deployment_group" "codedeploy_group_stack" {
   app_name              = var.app_name
-  deployment_group_name = join("-", [var.deployment_group_name, var.environment, var.name])
+  #deployment_group_name = join("-", [var.deployment_group_name, var.environment, var.name])
+  deployment_group_name = var.name
   service_role_arn      = var.service_role_arn
   autoscaling_groups    = var.autoscaling_groups 
 
